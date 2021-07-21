2c01c8d4f3f51c22d18d6308deff66fb2b29da85
neerajds1002 <87542408+neerajds1002@users.noreply.github.com>
2021-07-21 08:27:28 +0530

    Create basic_attribute.py
    Basic Attributes of the ndarray Class

---
+++ basic_attribute.py
@@ -0,0 +1,17 @@
+import numpy as np
+data = np.array([[1, 2], [3, 4], [5, 6]])
+
+#shape of the array(A tuple that contains the number of elements (i.e., the length) for each dimension (axis) of the array.)
+data.shape
+
+#size of the array(The total number elements in the array.)
+data.size
+
+#Number of dimensions (axes).
+data.ndim
+
+#Number of bytes used to store the data.
+data.nbytes
+
+#The data type of the elements in the array.
+data.dtype
